6664e6bcaaf9ea5aea0619320ff232c908b347e6
erik <erik@q32.com>
2021-07-22 14:56:41 -0400

    versIon

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def long_description():
 
 setup(
     name="atakama",
-    version="1.0.3",
+    version="1.0.4",
     description="Atakama sdk",
     packages=["atakama"],
     long_description=long_description(),
