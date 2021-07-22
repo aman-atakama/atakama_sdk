fd0a8a2037953cec949e380d2bae0cdceb17c1bd
erik <erik@q32.com>
2021-07-22 12:29:39 -0400

    tests

---
+++ tests/test_pluginlib.py
@@ -0,0 +1,33 @@
+from atakama import DetectorPlugin, FileChangedPlugin
+from atakama.plugin_base import Plugin
+
+def test_simple_detector():
+    # we just test that we can write a class to spec
+    class ExamplePlugin(DetectorPlugin):
+        @staticmethod
+        def name():
+            return "yo"
+
+        def needs_encryption(self, full_path):
+            return True
+
+    assert Plugin.get_by_name("yo") is ExamplePlugin
+
+    ExamplePlugin({"arg": 1})
+
+
+
+def test_simple_fchange():
+    # we just test that we can write a class to spec
+    class ExamplePlugin(FileChangedPlugin):
+        @staticmethod
+        def name():
+            return "yo"
+
+        def file_changed(self, full_path):
+            return
+
+    assert Plugin.get_by_name("yo") is ExamplePlugin
+    
+    ExamplePlugin({"arg": 1})
+
