bee03335ca1e689365d523ccfcbc76447de780c1
erik aronesty <erik@q32.com>
2022-03-11 11:57:57 -0500

    verbump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def long_description():
 
 setup(
     name="atakama",
-    version="1.4.0",
+    version="1.5.0",
     description="Atakama sdk",
     packages=["atakama"],
     long_description=long_description(),
