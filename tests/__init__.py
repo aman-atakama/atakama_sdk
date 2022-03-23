16b5cc6a7e71392d1c13f4f7531b5f5586ae078e
erik aronesty <erik@q32.com>
2022-03-23 12:34:36 -0400

    removeyear

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,2 @@
-# SPDX-FileCopyrightText: © 2020 Atakama, Inc <support@atakama.com>
+# SPDX-FileCopyrightText: © Atakama, Inc <support@atakama.com>
 # SPDX-License-Identifier: LGPL-3.0-or-later
--- tests/package/example.py
+++ tests/package/example.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: © 2020 Atakama, Inc <support@atakama.com>
+# SPDX-FileCopyrightText: © Atakama, Inc <support@atakama.com>
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
 from atakama import Plugin
